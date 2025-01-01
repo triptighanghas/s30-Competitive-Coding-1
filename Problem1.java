a6dda1de2123e358303bfbe230674fb9290e884a
Tripti Ghanghas <triptighanghas23@gmail.com>
2024-12-31 16:07:57 -0800

    competitive coding 1 done

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,24 @@
+//TC: O(log n)
+//SC: O(1) no extra space
+//Approach: binary search,
+// comparing at each mid if diff between low and mid is in sync with difference between each element should be
+// if yes, go to right, else go left
 
+class FindMissingNumber {
+    public static int findMissingNumber(int[] arr) {
+        int low = 0, high = arr.length - 1;
+        int commonDifference = (arr[high] - arr[low]) / arr.length;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == arr[0] + mid * commonDifference) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return arr[0] + low * commonDifference;
+    }
+
+}
--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,93 @@
+class MinHeap {
+    private final int[] heap;
+    private int size;
+    private final int capacity;
+
+    public MinHeap(int capacity) {
+        this.capacity = capacity;
+        this.size = 0;
+        this.heap = new int[capacity];
+    }
+
+    private int parent(int index) {
+        return (index - 1) / 2;
+    }
+
+    private int leftChild(int index) {
+        return (2 * index) + 1;
+    }
+
+    private int rightChild(int index) {
+        return (2 * index) + 2;
+    }
+
+    private boolean isFull() {
+        return size == capacity;
+    }
+
+    public void insert(int value) {
+        if (isFull()) {
+            throw new IllegalStateException("Heap is full");
+        }
+        heap[size] = value;
+        size++;
+        heapifyUp(size - 1);
+    }
+
+    private void heapifyUp(int index) {
+        while (index > 0 && heap[index] < heap[parent(index)]) {
+            swap(index, parent(index));
+            index = parent(index);
+        }
+    }
+
+    public int extractMin() {
+        if (size == 0) {
+            throw new IllegalStateException("Heap is empty");
+        }
+        int min = heap[0];
+        heap[0] = heap[size - 1];
+        size--;
+        heapifyDown(0);
+        return min;
+    }
+
+    private void heapifyDown(int index) {
+        int smallest = index;
+        int left = leftChild(index);
+        int right = rightChild(index);
+
+        if (left < size && heap[left] < heap[smallest]) {
+            smallest = left;
+        }
+        if (right < size && heap[right] < heap[smallest]) {
+            smallest = right;
+        }
+
+        if (smallest != index) {
+            swap(index, smallest);
+            heapifyDown(smallest);
+        }
+    }
+
+    public int getMin() {
+        if (size == 0) {
+            throw new IllegalStateException("Heap is empty");
+        }
+        return heap[0];
+    }
+
+    private void swap(int i, int j) {
+        int temp = heap[i];
+        heap[i] = heap[j];
+        heap[j] = temp;
+    }
+
+    public void printHeap() {
+        for (int i = 0; i < size; i++) {
+            System.out.print(heap[i] + " ");
+        }
+        System.out.println();
+    }
+}
 
